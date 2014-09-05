68a6fd2c3032350be5f6a880faa54c1801c47720
coupling <martijn@coupling.nl>
2014-09-05 13:00:28 +0200

    Initial project, with bootstrap, simple_form and devise.

---
+++ README.md
@@ -0,0 +1,3 @@
+# A rails admin interface with bootstrap, devise and simple_form 
+
+This should help you kickstart your rails admin application.
---
+++ config/initializers/assets.rb
@@ -0,0 +1,1 @@
+Rails.application.config.assets.precompile += %w( glyphicons-halflings-regular.eot glyphicons-halflings-regular.ttf glyphicons-halflings-regular.woff glyphicons-halflings-regular.svg)
